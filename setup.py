99e2c53344b536deee24957e63131f84cf3a48d7
Joshua Melander <melander@stanford.edu>
2024-02-19 17:14:21 -0800

    build

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='1.0',
     author='Joshua Melander, Javier Weddington, Youssef Faragalla',
     packages=find_packages(),
-    # entry_points={
-    #     'console_scripts': [
-    #         'mrdrphd=mrdrphd.run:main'
-    #     ]
-    # },
+    entry_points={
+        'console_scripts': [
+            'geezer=geezer.gui.main:main'
+        ]
+     },
     include_package_data=True,
     zip_safe=False,
 )
