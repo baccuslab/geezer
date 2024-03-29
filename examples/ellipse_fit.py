bb8bdc4fdd217df22d519b19238eefd0a06cc684
Youssef F <jafaragalla@gmail.com>
2024-03-29 22:38:07 +0000

    changes to include for ellipse fit

--- examples/ellipse_fit.py
+++ examples/ellipse_fit.py
@@ -32,6 +32,100 @@ Goal for this file:
 - Implement in existing code
 """
 
+def find_closest_centroid(image, threshold_value, point):
+    # Threshold the image
+    thresholded_image = np.where(image > threshold_value, 255, 0)
+
+    # Find connected components above the threshold
+    labels = measure.label(thresholded_image)
+    regions = measure.regionprops(labels) 
+
+    # Find the centroid coordinates of each connected component
+    centroids = []
+    for region in regions:
+        centroids.append(region.centroid)
+
+    # Find the closest centroid to the given point
+    closest_centroid = None
+    closest_distance = None
+    for centroid in centroids:
+        distance = np.sqrt((centroid[0] - point[1])**2 + (centroid[1] - point[0])**2)
+        if closest_centroid is None or distance < closest_distance:
+            closest_centroid = centroid
+            closest_distance = distance
+
+    return (closest_centroid[1], closest_centroid[0])
+
+def dogs(og_frame, pupil_params, fid_params):
+    print(pupil_params)
+    exp, gs, gl, thresh = pupil_params
+
+    gauss_small = gs
+    gauss_large = gl
+
+    dog = og_frame
+    #thresshold the large amplitude to for detecting the pupil 
+    dog = dog - np.min(dog)
+    dog = dog / np.max(dog)
+    dog = np.max(dog) - dog
+    dog = dog ** exp
+    dog = skimage.filters.gaussian(dog, sigma=gauss_small) - skimage.filters.gaussian(dog, sigma=gauss_large)
+    dog = dog - np.min(dog)
+    dog = dog / np.max(dog)
+    dog = dog * 255
+    dog = dog.astype(np.uint8)
+
+    pup_filtered = dog
+    
+    _, thresh = cv2.threshold(dog, thresh, 255, cv2.THRESH_BINARY)
+    pup_thresh = thresh
+    
+    exp, gs, gl, thresh = fid_params
+    gauss_small = gs
+    gauss_large = gl
+    dog = og_frame 
+    dog = dog - np.min(dog)
+    dog = dog / np.max(dog)
+    dog = dog ** exp
+    dog = skimage.filters.gaussian(dog, sigma=gauss_small) - skimage.filters.gaussian(dog, sigma=gauss_large)
+    dog = dog - np.min(dog)
+    dog = dog / np.max(dog)
+    dog = dog * 255
+    fid_filtered = dog.astype(np.uint8)
+
+    _, thresh = cv2.threshold(dog, thresh, 255, cv2.THRESH_BINARY)
+    fid_thresh = thresh.astype(np.uint8)
+
+    return pup_filtered, pup_thresh, fid_filtered, fid_thresh
+
+def process_frame(frame, pup, fids, pupil_params, fid_params, ellipse=False):
+    if ellipse:
+        # THIS IS CURRENTLY BRROKEN
+        pf, pt, ff, ft = dogs(frame, pupil_params, fid_params)
+        center = find_closest_centroid(pt, 100, pup)
+
+        pup_loc = pupil_locator(pt, center) #finds edges
+        pxy, width, height, phi = fit(pup_loc) #fits ellipse
+
+        final_fid_xys = {} 
+        for k,v in fids.items():
+            final_fid_xys[k] = find_closest_centroid(ft, 100, v)
+
+        return pxy, final_fid_xys, width, height, phi
+    else:
+        pf, pt, ff, ft = dogs(frame, pupil_params, fid_params)
+        pxy= find_closest_centroid(pt, 100, pup)
+        final_fid_xys = {} 
+        for k,v in fids.items():
+            final_fid_xys[k] = find_closest_centroid(ft, 100, v)
+
+        
+        width = None
+        height = None
+        phi = None
+
+        return pxy, final_fid_xys, width, height, phi
+
 
 
 
@@ -56,7 +150,7 @@ Focus on how to use this and find where the issue is in the process_image functi
 
             try:
                 frame = frame.mean(axis=2)
-                _processed_frame = geezer.process_frame(
+                _processed_frame = process_frame(
                     frame, pxy, fxys, proc_pup_params, proc_fid_params
                 )
                 
@@ -78,5 +172,5 @@ Focus on how to use this and find where the issue is in the process_image functi
 
     # Append local results to the shared list
     result_list.extend(local_results)
-
+    results = list(result_list)
 
