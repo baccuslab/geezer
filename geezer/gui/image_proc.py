415dc3051ea06e0c269a08357f28c2220c0efdf1
Joshua Melander <melander@stanford.edu>
2024-05-08 18:00:20 -0700

    much better

--- geezer/gui/image_proc.py
+++ geezer/gui/image_proc.py
@@ -410,8 +410,7 @@ class ImageProcTab(QWidget):
 
         print(self.mp4_filename)
         # run the following command in a separate process
-        command = 'ffmpeg -i {} -filter:v "crop={}:{}:{}:{}" -c:v ffv1 -level 3 -g 1 -coder 1 -context 1 -c:a copy {}'.format(
-                'ffmpeg -i {} -filter:v "crop=250:250:0:0" -c:v libx264 -crf 0 -preset ultrafast -c:a copy output.mp4',
+        command = 'ffmpeg -i {} -filter:v "crop={}:{}:{}:{}" -c:v h264 -crf 0 -preset ultrafast -c:a copy {}'.format(
 
             self.mp4_filename,
             int(x_end - x_start),
